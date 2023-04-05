8bf2b0f6a70c26723be19a7bd4131840d1edfacd
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-04-05 10:13:13 +0100

    useState implementation with objects and making use of rest[...] operator to make a copy of present object

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import HookCounter from "./FuncComponents/HookCounter";
+import HookCounterWithObjects from "./FuncComponents/HookCounterWithObjects";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <HookCounter />
+      <HookCounterWithObjects />
     </div>
   );
 }
