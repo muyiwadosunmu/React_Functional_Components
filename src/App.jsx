585548bbe823f484557d83a855eff98466b72fcd
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-04-05 14:56:56 +0100

    How useContext hook was being used before thr Context Hook came on board, though it didn't work here

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
+import { React, useState } from "react";
+
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import DataFetching from "./FuncComponents/DataFetching";
-function App() {
-  const [count, setCount] = useState(0);
+import ComponentC from "./FuncComponents/ComponentC";
+export const UserContext = React.createContext();
+export const ChannelContext = React.createContext();
 
+function App() {
   return (
     <div className="App">
-      <DataFetching />
+      <UserContext.Provider value={"Muyiwa"}>
+        <ChannelContext.Provider value={"Struct Technologies"}>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
     </div>
   );
 }
